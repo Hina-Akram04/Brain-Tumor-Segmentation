af855cd059cd6114980e2c8d626ed2c2dc58bbea
Hina-Akram04 <hinaakram0004@gmail.com>
2026-08-12 17:33:30 +0500

    model

---
+++ model.py
@@ -0,0 +1,54 @@
+import torch
+import torch.nn as nn
+
+
+def double_conv(in_ch, out_ch):
+    return nn.Sequential(
+        nn.Conv2d(in_ch, out_ch, 3, padding=1),
+        nn.BatchNorm2d(out_ch),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(out_ch, out_ch, 3, padding=1),
+        nn.BatchNorm2d(out_ch),
+        nn.ReLU(inplace=True),
+    )
+
+
+class UNet(nn.Module):
+    """Lightweight U-Net, sized down for CPU training."""
+
+    def __init__(self, in_channels=4, num_classes=4, base_ch=16):
+        super().__init__()
+        c1, c2, c3, c4, c5 = base_ch, base_ch * 2, base_ch * 4, base_ch * 8, base_ch * 16
+
+        self.enc1 = double_conv(in_channels, c1)
+        self.enc2 = double_conv(c1, c2)
+        self.enc3 = double_conv(c2, c3)
+        self.enc4 = double_conv(c3, c4)
+        self.bottleneck = double_conv(c4, c5)
+
+        self.pool = nn.MaxPool2d(2)
+
+        self.up4 = nn.ConvTranspose2d(c5, c4, 2, stride=2)
+        self.dec4 = double_conv(c5, c4)
+        self.up3 = nn.ConvTranspose2d(c4, c3, 2, stride=2)
+        self.dec3 = double_conv(c4, c3)
+        self.up2 = nn.ConvTranspose2d(c3, c2, 2, stride=2)
+        self.dec2 = double_conv(c3, c2)
+        self.up1 = nn.ConvTranspose2d(c2, c1, 2, stride=2)
+        self.dec1 = double_conv(c2, c1)
+
+        self.out_conv = nn.Conv2d(c1, num_classes, 1)
+
+    def forward(self, x):
+        e1 = self.enc1(x)
+        e2 = self.enc2(self.pool(e1))
+        e3 = self.enc3(self.pool(e2))
+        e4 = self.enc4(self.pool(e3))
+        b = self.bottleneck(self.pool(e4))
+
+        d4 = self.dec4(torch.cat([self.up4(b), e4], dim=1))
+        d3 = self.dec3(torch.cat([self.up3(d4), e3], dim=1))
+        d2 = self.dec2(torch.cat([self.up2(d3), e2], dim=1))
+        d1 = self.dec1(torch.cat([self.up1(d2), e1], dim=2))
+
+        return self.out_conv(d1)  # (B, num_classes, H, W) raw logits
